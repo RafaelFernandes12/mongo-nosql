3aa16c35acff56d45249ae15b3e412a3117df51d
Raquel Garcia <raqueldossantosgarcia@gmail.com>
2024-12-22 11:39:03 -0300

    feat: inicio post e fim get todas as entidades do banco

--- index.js
+++ index.js
@@ -13,26 +13,61 @@ mongoose.connect('mongodb://127.0.0.1:27017/users', {
 .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
 
-
+function createDynamicSchema(jsonInput){
+  const buildingDynamic = {}
+  Object.keys(jsonInput).forEach(key =>{
+    if(typeof jsonInput[key] == "string"){
+      console.log("a")
+      buildingDynamic[key] = {type: String}
+    }
+    if(typeof jsonInput[key] == "number"){
+      buildingDynamic[key] = Number
+    }
+  })
+  return buildingDynamic
+}
 
 app.post('/:dynamic', async (req, res)=>{
   const { dynamic } = req.params;
-  const collectionSchema = new mongoose.Schema(JSON.parse(req.body));
-  const User = mongoose.model({dynamic}, collectionSchema);
+
+  //extraindo os tipos do json da entidade criada
+  const schema = createDynamicSchema(req.body);
+  console.log(schema);
+  const collectionSchema = new mongoose.Schema(schema);
+  //books, {"name": "Poor things", "description": "an awesome book"}
+  const Entity = mongoose.model(`${dynamic}`, collectionSchema);
 
   try {
-    const everything = req.body;
-    const collection = new User(everything);
+
+    //criando o documento do post inicial
+    const collection = new Entity(req.body);
     const savedCollection = await collection.save();
 
 
     res.status(201).json(savedCollection);
+    console.log(savedCollection)
 
   } catch (err) {
-    console.error('Erro ao criar usuario:', err);
-    res.status(500).json({ error: 'Erro ao criar collection' });
+    res.status(500).json({ error: 'Erro ao criar entidade'});
+    console.log('Erro ao criar entidade');
   }
 });
+
+app.get('/', async(req, res)=>{
+  const collectionsName = await mongoose.connection.db.listCollections().toArray();
+  let everyCollection = "";
+  try{
+    for(value in collectionsName){
+      everyCollection = everyCollection + collectionsName[value].name + " "
+    }
+    res.send(everyCollection)
+    console.log(everyCollection)
+  }
+  catch(error){
+    res.status(500).json({ error: 'Erro ao criar entidade'});
+    console.log('Erro ao listar entidades do banco');
+  }
+})
 
 app.put('/users/:id', async (req, res) => {
   try {
