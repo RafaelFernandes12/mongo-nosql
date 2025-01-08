14d26e5035fad2222ef0daccd2a9bff19e4db329
RafaelFernandes12 <rfsilva786@gmail.com>
2025-01-08 16:38:11 -0300

    conserta o bug do create e put

--- index.js
+++ index.js
@@ -1,188 +1,182 @@
-const express = require('express');
-const req = require('express/lib/request');
-const mongoose = require('mongoose');
+const express = require("express");
+const req = require("express/lib/request");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/users', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Conexão com MongoDB bem-sucedida!'))
-.catch(err => console.error('Erro ao conectar ao MongoDB:', err));
-
-
-function createDynamicSchema(jsonInput){
-  const buildingDynamic = {}
-  Object.keys(jsonInput).forEach(key =>{
-    if(typeof jsonInput[key] == "string"){
-      buildingDynamic[key] = {type: String}
-    }
-    if(typeof jsonInput[key] == "number"){
-      buildingDynamic[key] = Number
-    }
+mongoose
+  .connect("mongodb://127.0.0.1:27017/users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  return buildingDynamic
+  .then(() => console.log("Conexão com MongoDB bem-sucedida!"))
+  .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
+
+function createDynamicSchema(jsonInput) {
+  const buildingDynamic = {};
+  Object.keys(jsonInput).forEach((key) => {
+    if (typeof jsonInput[key] == "string") {
+      buildingDynamic[key] = { type: String };
+    }
+    if (typeof jsonInput[key] == "number") {
+      buildingDynamic[key] = Number;
+    }
+  });
+  return buildingDynamic;
 }
 
-app.post('/:dynamic', async (req, res)=>{
+app.post("/:dynamic", async (req, res) => {
   const { dynamic } = req.params;
 
   //extraindo os tipos do json da entidade criada
   const schema = createDynamicSchema(req.body);
   const collectionSchema = new mongoose.Schema(schema);
   //books, {"name": "Poor things", "description": "an awesome book"}
-  const Entity = mongoose.model(`${dynamic}`, collectionSchema);
+  const Entity =
+    mongoose.models[dynamic] || mongoose.model(dynamic, collectionSchema);
 
   try {
-
     //criando o documento do post inicial
     const collection = new Entity(req.body);
     const savedCollection = await collection.save();
 
-
     res.status(201).json(savedCollection);
-    console.log(savedCollection)
-
+    console.log(savedCollection);
   } catch (err) {
-    res.status(500).json({ error: 'Erro ao criar entidade'});
-    console.log('Erro ao criar entidade');
+    res.status(500).json({ error: "Erro ao criar entidade" });
+    console.log("Erro ao criar entidade");
   }
 });
 
-app.get('/', async(req, res)=>{
-  try{
-    const collectionsName = await mongoose.connection.db.listCollections().toArray();
+app.get("/", async (req, res) => {
+  try {
+    const collectionsName = await mongoose.connection.db
+      .listCollections()
+      .toArray();
     let everyCollection = "";
-    for(value in collectionsName){
-      everyCollection = everyCollection + collectionsName[value].name + " "
+    for (value in collectionsName) {
+      everyCollection = everyCollection + collectionsName[value].name + " ";
     }
-    res.send(everyCollection)
-    console.log(collectionsName)
+    res.send(everyCollection);
+    console.log(collectionsName);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao listar entidades do banco" });
+    console.log("Erro ao listar entidades do banco");
   }
-  catch(error){
-    res.status(500).json({ error: 'Erro ao listar entidades do banco'});
-    console.log('Erro ao listar entidades do banco');
-  }
-})
-app.get('/:dynamic/:value', async(req, res)=>{
-  try{
-    const{dynamic, value} =  req.params
+});
+app.get("/:dynamic/:value", async (req, res) => {
+  try {
+    const { dynamic, value } = req.params;
     let objectId = new mongoose.Types.ObjectId(value);
-    const idSearched = await mongoose.connection.db.collection(dynamic).findOne({_id: objectId})
-    if(idSearched == null){
-      res.send("Nenhum documento foi encontrado")
+    const idSearched = await mongoose.connection.db
+      .collection(dynamic)
+      .findOne({ _id: objectId });
+    if (idSearched == null) {
+      res.send("Nenhum documento foi encontrado");
+    } else {
+      console.log(idSearched);
+      res.send(idSearched);
     }
-    else{
-      console.log(idSearched)
-      res.send(idSearched)
-    }
-  }catch(error){
-    res.status(500).json({ error: 'Erro ao listar por id'});
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao listar por id" });
     console.error(error);
   }
-})
+});
 
-
-app.put('/:dynamic/:id', async (req, res) => {
+app.put("/:dynamic/:id", async (req, res) => {
   const { dynamic, id } = req.params;
 
   //extraindo os tipos do json da entidade criada
   const schema = createDynamicSchema(req.body);
   const collectionSchema = new mongoose.Schema(schema);
   //books, {"name": "Poor things", "description": "an awesome book"}
-  const Entity = mongoose.model(`${dynamic}`, collectionSchema);
+  const Entity =
+    mongoose.models[dynamic] || mongoose.model(dynamic, collectionSchema);
 
   try {
-
-    const updatedObject = await Entity.findByIdAndUpdate(
-      id,
-      req.body
-    );
+    const updatedObject = await Entity.findByIdAndUpdate(id, req.body);
 
     res.send("Documento atualizado" + updatedObject);
-    console.log("Documento atualizado" + updatedObject)
-
+    console.log("Documento atualizado" + updatedObject);
   } catch (err) {
-    res.status(500).json({ error: 'Erro ao atualizar documento'});
-    console.error('Erro ao atualizar documento', err);
+    res.status(500).json({ error: "Erro ao atualizar documento" });
+    console.error("Erro ao atualizar documento", err);
   }
-})
-function createFieldsJson(fieldsInput){
+});
+function createFieldsJson(fieldsInput) {
   //name, age => {fields: {name:1}, {age:1}}
   //-name, age => {fields: {name:0}, {age:1}}
 
-  const list = fieldsInput.split(",")
-  const buildingFieldsJson = {projection:{}}
-  
-  for(value in list){
-    console.log(value)
-    if(list[value].indexOf("-")!=-1){
-      buildingFieldsJson.projection[list[value].substring(1,list[value].length)] = 0
-    }
-    else{
-      buildingFieldsJson.projection[list[value]] = 1
+  const list = fieldsInput.split(",");
+  const buildingFieldsJson = { projection: {} };
+
+  for (value in list) {
+    console.log(value);
+    if (list[value].indexOf("-") != -1) {
+      buildingFieldsJson.projection[
+        list[value].substring(1, list[value].length)
+      ] = 0;
+    } else {
+      buildingFieldsJson.projection[list[value]] = 1;
     }
   }
-  return buildingFieldsJson
+  return buildingFieldsJson;
 }
 //buscaPaginada ou Projeção
-app.get('/:dynamic', async (req, res) => {
-    const {dynamic} = req.params
-    if(req.query.query == undefined){
+app.get("/:dynamic", async (req, res) => {
+  const { dynamic } = req.params;
+  if (req.query.query == undefined) {
+    //retornando query com os fields a serem apresentados
+    const fields = req.query.fields;
+    const fieldsJson = createFieldsJson(fields);
 
-      //retornando query com os fields a serem apresentados
-      const fields = req.query.fields;
-      const fieldsJson = createFieldsJson(fields)
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = parseInt(req.query.skip) || 0;
 
-      const limit = parseInt(req.query.limit) || 10; 
-      const skip = parseInt(req.query.skip) || 0;
-
-      const projecao = await mongoose.connection.db.collection(dynamic)
-      .find({}, fieldsJson)      
+    const projecao = await mongoose.connection.db
+      .collection(dynamic)
+      .find({}, fieldsJson)
       .limit(limit)
       .skip(skip)
       .toArray();
 
-      if(projecao.length==0){
-        res.send("Nenhum elemento retornado")
+    if (projecao.length == 0) {
+      res.send("Nenhum elemento retornado");
+    } else {
+      const everyItem = [];
+      for (index in projecao) {
+        console.log(projecao[index]);
+        everyItem.push(projecao[index]);
       }
-      else{
-        const everyItem = []
-        for(index in projecao){
-            console.log(projecao[index])
-            everyItem.push(projecao[index])
-        }
-        res.send(everyItem)
-      }
+      res.send(everyItem);
     }
-    else{
-      try{
-        const query = req.query.query ? JSON.parse(req.query.query) : {};
-        const limit = parseInt(req.query.limit) || 10; 
-        const skip = parseInt(req.query.skip) || 0;   
-    
-        const buscaPaginada = await mongoose.connection.db.collection(dynamic)
-          .find(query)
-          .limit(limit)
-          .skip(skip)
-          .toArray();
-    
-        if(buscaPaginada.length==0){
-          res.send("Nenhum elemento retornado")
+  } else {
+    try {
+      const query = req.query.query ? JSON.parse(req.query.query) : {};
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = parseInt(req.query.skip) || 0;
+
+      const buscaPaginada = await mongoose.connection.db
+        .collection(dynamic)
+        .find(query)
+        .limit(limit)
+        .skip(skip)
+        .toArray();
+
+      if (buscaPaginada.length == 0) {
+        res.send("Nenhum elemento retornado");
+      } else {
+        const everyItem = [];
+        for (index in buscaPaginada) {
+          console.log(buscaPaginada[index]);
+          everyItem.push(buscaPaginada[index]);
         }
-        else{
-          const everyItem = []
-          for(index in buscaPaginada){
-              console.log(buscaPaginada[index])
-              everyItem.push(buscaPaginada[index])
-          }
-          res.send(everyItem)
-        }
-      }catch (err) {
-      res.status(500).json({err});
+        res.send(everyItem);
+      }
+    } catch (err) {
+      res.status(500).json({ err });
       console.error(err);
     }
   }
